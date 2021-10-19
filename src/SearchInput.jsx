60143da0c1994e3525ec662b1ee0545d82eb82b8
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-10-19 14:41:18 +0300

    add event handler to SearchInput component

--- src/SearchInput.jsx
+++ src/SearchInput.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const SearchInput = () => (
-  <input />
-);
+const SearchInput = () => {
+  const handleChange = () => {
+    console.log('You changed the search input.');
+  };
+
+  return (
+    <input
+      onChange={handleChange}
+    />
+  );
+};
 
 export default SearchInput;
