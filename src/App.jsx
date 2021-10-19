ae891dc04689aca3593954d250e81c8bb039fc55
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-10-19 11:58:57 +0300

    add App component

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Emoji Search</h2>
+    </div>
+  </div>
+);
+
+export default App;
