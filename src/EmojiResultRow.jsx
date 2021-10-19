f6595126e4cea53bd7cef5712d57df24882854a9
Vladimir Loskutov <vladimirloskutov@icloud.com>
2021-10-19 12:47:10 +0300

    update EmojiResultRow component

--- src/EmojiResultRow.jsx
+++ src/EmojiResultRow.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const EmojiResultRow = () => (
-  <div />
+  <div>
+    <img
+      alt="Grinning"
+      src="//cdn.jsdelivr.net/emojione/assets/png/1f600.png"
+    />
+    <span>
+      Grinning
+    </span>
+  </div>
 );
 
 export default EmojiResultRow;
